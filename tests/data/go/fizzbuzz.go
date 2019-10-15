bc3189adec92c2f7387fcb14b2e2eaa921594ede
Sergi Chalauri <sergi.chalauri@gmail.com>
2019-10-15 17:13:59 +0400

    Simple go file to test

---
+++ tests/data/go/fizzbuzz.go
@@ -0,0 +1,11 @@
+package main
+ 
+import "fmt"
+// Only formatting is messed up
+func main() {
+for i := 1; i <= 100; i++ {
+fmt.Println(map[bool]map[bool]interface{}{
+false: {false: i, true: "Fizz"}, true: {false: "Buzz", true: "FizzBuzz"},
+}[i%5 == 0][i%3 == 0])
+}
+}
